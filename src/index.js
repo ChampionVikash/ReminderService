0069d61ab0592a22bb8277a44601d7d422596802
ChampionVikash <4355vikashkumar4355@gmail.com>
2023-06-07 03:57:40 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -3,29 +3,28 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-// const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
 
-const cron = require('node-cron');
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
-const setupAndStartServer = () => {
-     const app = express();
-     app.use(bodyParser.json());
-     app.use(bodyParser.urlencoded({ extended: true }));
+const setupAndStartServer = async () => {
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-     app.listen(PORT, () => {
+    app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
+    app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-
-      //   sendBasicEmail(
-      //      'support@admin.com',
-      //      'sankat@gmail.com',
-      //      'This is a testing email',
-      //      'Hey, how are you, I hope you like the support'
-      //   )
         
-        cron.schedule('*/2 * * * *',() => {
-           console.log('running a task every two minutes');
-        });
-     });
+    });
 }
 
 setupAndStartServer();
+
--- src/models/notificationticket.js
+++ src/models/notificationticket.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class NotificationTicket extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+ 
     static associate(models) {
-      // define association here
+     
     }
   }
   NotificationTicket.init({
@@ -29,7 +25,8 @@ module.exports = (sequelize, DataTypes) => {
     status: {
       type: DataTypes.ENUM,
       allowNull: false,
-      values: ["PENDING", "SUCCESS", "FAILED"]
+      values: ["PENDING", "SUCCESS", "FAILED"],
+      defaultValue: "PENDING"
     },
     notificationTime: {
       type: DataTypes.DATE,
